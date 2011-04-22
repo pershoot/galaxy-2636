a02356782847a9f1c254e879960987a776bb17f6
Rafael J. Wysocki <rjw@sisk.pl>
2011-04-22 22:02:33 +0200

    ARM: Use struct syscore_ops instead of sysdevs for PM in common code
    Convert some ARM architecture's common code to using
    struct syscore_ops objects for power management instead of sysdev
    classes and sysdevs.
    
    This simplifies the code and reduces the kernel's memory footprint.
    It also is necessary for removing sysdevs from the kernel entirely in
    the future.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Greg Kroah-Hartman <gregkh@suse.de>
    
    Conflicts:
    
    	arch/arm/kernel/leds.c

--- arch/arm/kernel/leds.c
+++ arch/arm/kernel/leds.c
@@ -12,6 +12,7 @@
 #include <linux/notifier.h>
 #include <linux/cpu.h>
 #include <linux/sysdev.h>
+#include <linux/syscore_ops.h>
 
 #include <asm/leds.h>
 
@@ -71,34 +72,35 @@ static ssize_t leds_store(struct sys_device *dev,
 
 static SYSDEV_ATTR(event, 0200, NULL, leds_store);
 
-static int leds_suspend(struct sys_device *dev, pm_message_t state)
+static struct sysdev_class leds_sysclass = {
+	.name    = "leds",
+};
+
+static struct sys_device leds_device = {
+	.id    = 0,
+	.cls    = &leds_sysclass,
+};
+
+static int leds_suspend(void)
 {
 	leds_event(led_stop);
 	return 0;
 }
 
-static int leds_resume(struct sys_device *dev)
+static void leds_resume(void)
 {
 	leds_event(led_start);
-	return 0;
 }
 
-static int leds_shutdown(struct sys_device *dev)
+static void leds_shutdown(void)
 {
 	leds_event(led_halted);
-	return 0;
 }
 
-static struct sysdev_class leds_sysclass = {
-	.name		= "leds",
+static struct syscore_ops leds_syscore_ops = {
 	.shutdown	= leds_shutdown,
 	.suspend	= leds_suspend,
 	.resume		= leds_resume,
-};
-
-static struct sys_device leds_device = {
-	.id		= 0,
-	.cls		= &leds_sysclass,
 };
 
 static int leds_idle_notifier(struct notifier_block *nb, unsigned long val,
@@ -128,6 +130,8 @@ static int __init leds_init(void)
 		ret = sysdev_register(&leds_device);
 	if (ret == 0)
 		ret = sysdev_create_file(&leds_device, &attr_event);
+	if (ret == 0)
+		register_syscore_ops(&leds_syscore_ops);
 	if (ret == 0)
 		idle_notifier_register(&leds_idle_nb);
 
