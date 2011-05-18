7d481f0d70c9d5a62cb92be194ebcfc2760d1f77
Jean Delvare <khali@linux-fr.org>
2011-05-18 16:49:24 +0200

    Haavard Skinnemoen has left Atmel
    Haavard's e-mail address at Atmel is no longer valid.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Acked-by: Havard Skinnemoen <hskinnemoen@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    (cherry picked from commit e05503ef1186ad33dfe56794407891eb1dd93ef6)

--- drivers/net/macb.c
+++ drivers/net/macb.c
@@ -1346,5 +1346,5 @@ module_exit(macb_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Atmel MACB Ethernet driver");
-MODULE_AUTHOR("Haavard Skinnemoen <hskinnemoen@atmel.com>");
+MODULE_AUTHOR("Haavard Skinnemoen (Atmel)");
 MODULE_ALIAS("platform:macb");
