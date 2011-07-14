102de3cacb08b8452b45f3f2369f4acd86703897
Colin Cross <ccross@android.com>
2011-07-14 16:36:40 -0400

    mmc: host: sdhci: Resolve incorrect merge
    Change-Id: Ic91f35fbb1f0a131d8ab56817f0ad91288c82402
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1921,8 +1921,6 @@ int sdhci_add_host(struct sdhci_host *host)
 	if (host->quirks & SDHCI_QUIRK_RUNTIME_DISABLE)
 		mmc->caps |= MMC_CAP_DISABLE;
 
-	mmc->caps |= MMC_CAP_ERASE;
-
 	mmc->ocr_avail = 0;
 	if (caps & SDHCI_CAN_VDD_330)
 		mmc->ocr_avail |= MMC_VDD_32_33|MMC_VDD_33_34;
