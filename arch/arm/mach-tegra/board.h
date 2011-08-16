a3bda92b79b73736e446cc36e6b6020dd1f6b666
Alok Chauhan <alokc@nvidia.com>
2011-08-16 14:35:42 +0530

    arm: tegra: Added I2C arbitration lost recovery mechanism
    Added the code for arbitration lost recovery mechanism for i2c
    driver and Initialize gpio number for i2c clock and data as
    part of platform data.
    
    bug 854305
    
    This is cherry pick of change http://git-master/r/#change,43200
    in main but hand-merged.
    
    Change-Id: I2bf46cf54c04b94174435f3fb9c965b74156dd02
    Reviewed-on: http://git-master/r/46288
    Reviewed-by: Alok Chauhan <alokc@nvidia.com>
    Tested-by: Alok Chauhan <alokc@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -34,6 +34,7 @@ void tegra_move_framebuffer(unsigned long to, unsigned long from,
 	unsigned long size);
 int tegra_dvfs_rail_disable_by_name(const char *reg_id);
 bool is_tegra_debug_uartport_hs(void);
+int arb_lost_recovery(int scl_gpio, int sda_gpio);
 
 extern unsigned long tegra_bootloader_fb_start;
 extern unsigned long tegra_bootloader_fb_size;
