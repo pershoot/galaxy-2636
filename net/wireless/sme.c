7d7c91e9ba08f5f5e3f255b7a7543829838d18cf
Dmitry Shmidt <dimitrysh@google.com>
2011-10-18 12:30:02 -0700

    net: wireless: Fix CFG80211_ALLOW_RECONNECT option for disconnect
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- net/wireless/sme.c
+++ net/wireless/sme.c
@@ -647,8 +647,10 @@ void __cfg80211_disconnected(struct net_device *dev, const u8 *ie,
 	if (WARN_ON(wdev->iftype != NL80211_IFTYPE_STATION))
 		return;
 
+#ifndef CONFIG_CFG80211_ALLOW_RECONNECT
 	if (wdev->sme_state != CFG80211_SME_CONNECTED)
 		return;
+#endif
 
 	if (wdev->current_bss) {
 		cfg80211_unhold_bss(wdev->current_bss);
