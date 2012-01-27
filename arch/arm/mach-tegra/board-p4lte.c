f8ce8c92c178fb41eb7f0dfef477c0752e7e22af
Prashant Somashekar <prashsomash@gmail.com>
2012-01-26 19:39:52 -0500

    arch: arm: tegra: modify pll_a clock and uartd's parent
    -p4wifi: uartd's parent to pll_p and pll_a to 56448000
    -p4/p4lte: pll_a to 56448000
    -reference ventana

--- arch/arm/mach-tegra/board-p4lte.c
+++ arch/arm/mach-tegra/board-p4lte.c
@@ -453,7 +453,7 @@ static __initdata struct tegra_clk_init_table p3_clk_init_table[] = {
 	{ "pll_p_out4",	"pll_p",	24000000,	true },
 	{ "pwm",	"clk_32k",	32768,		false},
 	{ "pll_c",	"clk_m",	586000000,	true},
-	{ "pll_a",	NULL,		11289600,	true},
+	{ "pll_a",      NULL,           56448000,       true},
 	{ "pll_a_out0",	NULL,		11289600,	true},
 	{ "clk_dev1",   "pll_a_out0",   0,              true},
 	{ "i2s1",	"pll_a_out0",	11289600,	true},
