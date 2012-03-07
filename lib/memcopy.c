6184aa7b1346b79a204ff353647e9a824fc950de
Ezekeel <notezekeel@googlemail.com>
2012-03-07 07:52:44 +0100

    Added improved glibc memcpy and memmove functions. (cherry picked from commit dc916ac7608c268636c10b10a95a12df6bdcc579)
    Conflicts:
    
    	include/linux/memcopy.h
    	lib/memcopy.c

--- lib/memcopy.c
+++ lib/memcopy.c
@@ -3,7 +3,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
+ * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
  * 
  * This program is distributed in the hope that it will be useful,
@@ -13,7 +13,7 @@
  * 
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  *
  * The code is derived from the GNU C Library.
  * Copyright (C) 1991, 1992, 1993, 1997, 2004 Free Software Foundation, Inc.
